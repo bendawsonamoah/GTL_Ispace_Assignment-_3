ce224d0f4bd4cc97d97b751ed003cc012f2294ad
bendawsonamoah <68850246+bendawsonamoah@users.noreply.github.com>
2020-08-11 15:01:02 +0000

    Initial Commit
    GTL React Assignment

---
+++ HW3_W16GDA5I6X/precache-manifest.97907c4f8e17910e0d745047e95bcda7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de01975c62638447f25458a6bfe9be7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1874d0808e069518ed9",
+    "url": "/static/css/main.4c26b541.chunk.css"
+  },
+  {
+    "revision": "be2e8af8a1637ef5c9ba",
+    "url": "/static/js/2.9e159a32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9e159a32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1874d0808e069518ed9",
+    "url": "/static/js/main.31c42a98.chunk.js"
+  },
+  {
+    "revision": "001b1ed3bf6da95ddba5",
+    "url": "/static/js/runtime-main.0c8ed744.js"
+  },
+  {
+    "revision": "5f1cd582c6d4fb5f4d86e6c1635deec2",
+    "url": "/static/media/hulk-1.5f1cd582.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "294dd39b7ab19aedb004eb8f359c307f",
+    "url": "/static/media/venom-1.294dd39b.jpg"
+  },
+  {
+    "revision": "5eb62fe8d11e8a32b2fb2554088f308e",
+    "url": "/static/media/venom-2.5eb62fe8.jpg"
+  }
+]);
